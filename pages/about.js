750e85f7655aced38709cddbc40506c0e6e39e90
lordkonst <nkonstan.gr@gmail.com>
2021-04-19 19:26:41 +0300

    initial

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About page</h1>
+      <p>Thjis is an app to find the last DJ and other musical events</p>
+      <p>ver 1.0.0</p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ pages/events/add.js
@@ -0,0 +1,9 @@
+const AddEventPage = () => {
+  return (
+    <div>
+      <h1>Add event page</h1>
+    </div>
+  );
+};
+
+export default AddEventPage;
