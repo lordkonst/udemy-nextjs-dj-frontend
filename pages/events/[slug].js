ae38bb8242c529b7d06077b9a21b7d229c8568b6
lordkonst <nkonstan.gr@gmail.com>
2021-04-23 21:03:59 +0300

    data fetching. before strapi

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,12 +1,94 @@
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
+import styles from '@/styles/Event.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+import { FaPencilAlt, FaTimes } from 'react-icons/fa';
 
-const EventPage = () => {
+const EventPage = ({ evt }) => {
+  const deleteEvent = e => {
+    console.log('delete');
+  };
   const title = 'Event title';
   return (
     <Layout title={title}>
-      <h1>My event</h1>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt.id}`}>
+            <a>
+              <FaPencilAlt /> Edit event
+            </a>
+          </Link>
+          <a href="#" className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Delete Event
+          </a>
+        </div>
+        <span>
+          {evt.date} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+        {evt.image && (
+          <div>
+            <Image src={evt.image} width={960} height={600} />
+          </div>
+        )}
+        <h3>Performers</h3>
+        <p>{evt.performers}</p>
+        <h3>Description</h3>
+        <p>{evt.description}</p>
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        <Link href="/events">
+          <a className={styles.back}>{'< '}Go back</a>
+        </Link>
+      </div>
     </Layout>
   );
 };
 
 export default EventPage;
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  const paths = events.map(evt => ({
+    params: { slug: evt.slug },
+  }));
+
+  // must return an array of objects and fallback
+  // object must be {params:{identifier:1}}
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(ctx) {
+  const {
+    params: { slug },
+  } = ctx;
+
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+  return {
+    props: {
+      evt: events[0],
+    },
+  };
+}
+
+// export async function getServerSideProps(ctx) {
+//   const {
+//     query: { slug },
+//   } = ctx;
+
+//   const res = await fetch(`${API_URL}/api/events/${slug}`);
+//   const events = await res.json();
+//   return {
+//     props: {
+//       evt: events[0],
+//     },
+//   };
+// }
